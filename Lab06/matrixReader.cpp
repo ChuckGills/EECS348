270b8b23cdaf852a0e9d8cfb034368adbf08a2dd
ChuckGills <charliegillund@gmail.com>
2023-10-20 23:38:35 -0500

    added Lab06

---
+++ Lab06/matrixReader.cpp
@@ -0,0 +1,104 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    std::ifstream file("matrix.txt");
+    std::string container;
+    
+    
+    int size;
+    file >> size;
+    int mat1[size][size];
+    int mat2[size][size];
+    
+
+    int data[100];
+    int count = 0;
+
+    int number;
+    while (file >> number) {
+            data[count] = number;
+            count++;
+    }
+    //builds mat1
+    for(int i =0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            mat1[i][j]=data[3*i+j];
+        }
+    }
+    for(int i = 0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            mat2[i][j]=data[3*i+j+9];
+        }
+    }
+    std:: cout<< "Charlie Gillund"<<endl<<"Lab06 Matrix Manipulation"<<endl;
+    std::cout<<"Matrix 1: "<<endl<<mat1[0][0]<<" "<<mat1[0][1]<<" "<<mat1[0][2]<<endl;
+    std::cout<<mat1[1][0]<<" "<<mat1[1][1]<<" "<<mat1[1][2]<<endl;
+    std::cout<<mat1[2][0]<<" "<<mat1[2][1]<<" "<<mat1[2][2]<<endl;
+
+    std::cout<<"Matrix 2: "<<endl<<mat2[0][0]<<" "<<mat2[0][1]<<" "<<mat2[0][2]<<endl;
+    std::cout<<mat2[1][0]<<" "<<mat2[1][1]<<" "<<mat2[1][2]<<endl;
+    std::cout<<mat2[2][0]<<" "<<mat2[2][1]<<" "<<mat2[2][2]<<endl;
+    
+    
+    file.close();
+     int addmat[size][size];
+     int submat[size][size];
+     int multmat[size][size];
+    //adds matrix
+    for(int i =0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            addmat[i][j]=mat1[i][j]+mat2[i][j];
+        }
+    }
+    //prints matrix
+    std::cout<<endl<<"Matrix 1 + Matrix 2: "<<endl<<setw(3)<<addmat[0][0]<<" "<<setw(3)<<addmat[0][1]<<" "<<setw(3)<<addmat[0][2]<<endl;
+    std::cout<<setw(3)<<addmat[1][0]<<" "<<setw(3)<<addmat[1][1]<<" "<<setw(3)<<addmat[1][2]<<endl;
+    std::cout<<setw(3)<<addmat[2][0]<<" "<<setw(3)<<addmat[2][1]<<" "<<setw(3)<<addmat[2][2]<<endl;
+    //matrix multiplication
+    for(int i =0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+            multmat[i][j]=0;
+            for(int k=0; k<size;k++)
+            {
+                
+                multmat[i][j]=multmat[i][j]+(mat1[i][k]*mat2[k][j]);
+                
+            }
+            
+        }
+    }
+    //prints matrix
+    std::cout<<endl<<"Matrix 1 X Matrix 2: "<<endl<<setw(3)<<multmat[0][0]<<" "<<setw(3)<<multmat[0][1]<<" "<<setw(3)<<multmat[0][2]<<endl;
+    std::cout<<setw(3)<<multmat[1][0]<<" "<<setw(3)<<multmat[1][1]<<" "<<setw(3)<<multmat[1][2]<<endl;
+    std::cout<<setw(3)<<multmat[2][0]<<" "<<setw(3)<<multmat[2][1]<<" "<<setw(3)<<multmat[2][2]<<endl;
+    //subtracts matrix
+    for(int i =0;i<size;i++)
+    {
+        for(int j=0;j<size;j++)
+        {
+        submat[i][j]=mat1[i][j]-mat2[i][j];
+        }
+    }
+    std::cout<<endl<<"Matrix 1 - Matrix 2: "<<endl<<setw(3)<<submat[0][0]<<" "<<setw(3)<<submat[0][1]<<" "<<setw(3)<<submat[0][2]<<endl;
+    std::cout<<setw(3)<<submat[1][0]<<" "<<setw(3)<<submat[1][1]<<" "<<setw(3)<<submat[1][2]<<endl;
+    std::cout<<setw(3)<<submat[2][0]<<" "<<setw(3)<<submat[2][1]<<" "<<setw(3)<<submat[2][2]<<endl;
+
+    
+    
+
+   
+    return 0;
+}
